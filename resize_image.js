85d5f29b8808550abf29963d92a3e6dde5e96bbb
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-11-22 09:51:37 -0200

    ok

--- resize_image.js
+++ resize_image.js
@@ -1,17 +1,31 @@
 #!/usr/bin/env node
 
 var gm = require('gm');
+var ap = require('argparser').files(0).nums("w", "h").vals(1).parse();
 
-function resize(imagePath, width, height) {
+var src_path = ap.arg(0)
+var dst_path = ap.arg(1)
+var width = ap.opt("w")
+var height = ap.opt("h")
 
-  gm(imagePath)
+
+function resize(src_image, dst_image, width, height) {
+
+  console.log("Resizing image: " + src_image);
+  console.log("New Width: " + width);
+  console.log("New Height: " + height);
+  console.log("Saving at: " + dst_image);
+
+  gm(src_image)
   .resize(width, height)
     .autoOrient()
-    .write(writeStream, function (err) {
+    .write(dst_image, function (err) {
         if (!err) console.log(' hoora! ');
     });
 }
 
-function main(args) {
-  console.log(args)
+function main(src, dst, width, height) {
+  resize(src, dst, width, height)
 }
+
+main(src_path, dst_path, width, height)
