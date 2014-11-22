9ea35a42e800b4eb9a36447581efe11fd1efb99b
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-11-22 10:37:42 -0200

    ok

--- generate_icons.js
+++ generate_icons.js
@@ -1,4 +1,18 @@
+#!/usr/bin/env node
 
+var resizer = require("./resize_image");
 
-var ir = require("./resize_image")
+var ap = require('argparser').files(0).nonvals("ios", "android").parse();
 
+var src_path = ap.arg(0);
+var ios = ap.opt("ios");
+var android = ap.opt("android");
+
+if (ios) {
+  var prefix = "ios_icon_";
+  resizer.resize(src_path, "./" + prefix + "iPhone_6_Plus_@3x.png", 180, 180)
+  resizer.resize(src_path, "./" + prefix + "iPhone_6_And_5_@2x.png", 120, 120)
+  resizer.resize(src_path, "./" + prefix + "iPhone_4s_@2x.png", 120, 120)
+  resizer.resize(src_path, "./" + prefix + "iPad_And_iPad_Mini_@2x.png", 152, 152)
+  resizer.resize(src_path, "./" + prefix + "iPad2_And_iPad_Mini_@1x.png", 76, 76)
+}
