2aec183cbbf077eb479dac54044b44a75b79a6fa
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2014-11-22 10:40:08 -0200

    ok

--- generate_icons.js
+++ generate_icons.js
@@ -10,9 +10,10 @@ var android = ap.opt("android");
 
 if (ios) {
   var prefix = "ios_icon_";
-  resizer.resize(src_path, "./" + prefix + "iPhone_6_Plus_@3x.png", 180, 180)
-  resizer.resize(src_path, "./" + prefix + "iPhone_6_And_5_@2x.png", 120, 120)
-  resizer.resize(src_path, "./" + prefix + "iPhone_4s_@2x.png", 120, 120)
-  resizer.resize(src_path, "./" + prefix + "iPad_And_iPad_Mini_@2x.png", 152, 152)
-  resizer.resize(src_path, "./" + prefix + "iPad2_And_iPad_Mini_@1x.png", 76, 76)
+  resizer.resize(src_path, "./" + prefix + "iPhone_6_Plus_180x180_@3x.png", 180, 180)
+  resizer.resize(src_path, "./" + prefix + "iPhone_6_And_5_120x120_@2x.png", 120, 120)
+  resizer.resize(src_path, "./" + prefix + "iPhone_4s_120x120_@2x.png", 120, 120)
+  resizer.resize(src_path, "./" + prefix + "iPad_And_iPad_Mini_152x152_@2x.png", 152, 152)
+  resizer.resize(src_path, "./" + prefix + "iPad2_And_iPad_Mini_76x76_@1x.png", 76, 76)
+  resizer.resize(src_path, "./" + prefix + "Apple_Store_1024x1024.png", 1024, 1024)
 }
